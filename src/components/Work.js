6a134eed1c48bc729132b35dc6050fa3ed84a154
Tolok Oleksandr <tolok.oleksandr@gmail.com>
2024-01-05 16:31:46 +0100

    Created Work component

---
+++ src/components/Work.js
@@ -0,0 +1,161 @@
+/** @format */
+import BoraBora from "../assets/projects/borabora.jpg";
+import Tesla from "../assets/projects/model-y.jpg";
+import Food from "../assets/projects/food.avif";
+import Suitcase from "../assets/projects/suitcase.jpg";
+import Bill from "../assets/projects/bill.jpg";
+
+const Work = () => {
+  return (
+    <div
+      name='work'
+      className='w-full md:h-screen text-gray-300  bg-[#0a192f] '>
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+        <div className='pb-8'>
+          <p className='text-3xl font-bold inline border-b-4 border-[#B9464C]'>
+            Work
+          </p>
+          <p className='py-6'>Check out some of my recent work</p>
+        </div>
+
+        {/* Work container */}
+
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+          {/* Grid Item */}
+
+          <div
+            style={{ backgroundImage: `url(${Bill})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100 '>
+              <span className='text-2xl font-bold text-white tracking-wide'>
+                Eat & Split
+              </span>
+              <div className='pt-8 text-center'>
+                <a
+                  href='https://alex197925.github.io/eat-n-split-react/'
+                  target='blank'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href='https://github.com/alex197925/eat-n-split-react'
+                  target='blank'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    GitHub
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div
+            style={{ backgroundImage: `url(${Suitcase})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100 '>
+              <span className='text-2xl font-bold text-white tracking-wide'>
+                Travel List
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='https://faravawtravellist.netlify.app/' target='blank'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href='https://github.com/alex197925/travel-list-react'
+                  target='blank'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    GitHub
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div
+            style={{ backgroundImage: `url(${Food})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100 '>
+              <span className='text-2xl font-bold text-white tracking-wide'>
+                Best Eats
+              </span>
+              <div className='pt-8 text-center'>
+                <a
+                  href='https://alex197925.github.io/food-app-react/'
+                  target='blank'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href='https://github.com/alex197925/food-app-react'
+                  target='blank'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    GitHub
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div
+            style={{ backgroundImage: `url(${BoraBora})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100 '>
+              <span className='text-2xl font-bold text-white tracking-wide'>
+                First Class Travel
+              </span>
+              <div className='pt-8 text-center'>
+                <a
+                  href='https://travel-react-tailwind.netlify.app/'
+                  target='blank'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href='https://github.com/alex197925/react-tailwind/tree/main'
+                  target='blank'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    GitHub
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${Tesla})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100 '>
+              <span className='text-2xl font-bold text-white tracking-wide'>
+                Tesla Clone
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='https://tesla-clone-ea202.web.app/' target='blank'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href='https://github.com/alex197925/tesla-clone'
+                  target='blank'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    GitHub
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Work;
