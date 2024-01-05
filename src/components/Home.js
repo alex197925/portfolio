630d2aa1b13932fc4bd31440490d1d06d9e2c13b
Tolok Oleksandr <tolok.oleksandr@gmail.com>
2024-01-05 15:42:21 +0100

    Deploy test-4

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,15 @@
 /** @format */
 
 import React from "react";
+import Html from "../assets/html.png";
 
 const Home = () => {
-  return <div className='text-2xl text-red-900'>Home Updating</div>;
+  return (
+    <div className='text-2xl text-red-900'>
+      Home Updating
+      <img src={Html} alt='html' />
+    </div>
+  );
 };
 
 export default Home;
