0ecec6885891fff41e8e182ea7a6a180feb103df
Tolok Oleksandr <tolok.oleksandr@gmail.com>
2024-01-05 14:34:30 +0100

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+/** @format */
+
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className='w-full h-[80px] flex items-center justify-between text-gray-300 px-4 bg-[#0a192f] '></div>
+  );
+};
+
+export default Navbar;
