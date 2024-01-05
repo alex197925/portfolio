5b782daece151420e7f4b01abb35dce905e2491e
Tolok Oleksandr <tolok.oleksandr@gmail.com>
2024-01-05 15:38:35 +0100

    Deploy test-3

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@
 import React from "react";
 
 const Home = () => {
-  return <div className='text-2xl text-red-900'>Home</div>;
+  return <div className='text-2xl text-red-900'>Home Updating</div>;
 };
 
 export default Home;
